3739f39fc5ac32afb6e05babe47508b9931249ff
Dkasim0313 <Dkasim0313@gmail.com>
2016-09-06 17:35:49 +0800

    productdo新增 增加

--- src/tw/iii/qr/ProductDo.java
+++ src/tw/iii/qr/ProductDo.java
@@ -40,6 +40,7 @@ public class ProductDo extends HttpServlet {
 		}
 	}
 
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		try {
