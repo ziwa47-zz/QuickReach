b2489a0bee7f6cbc77877980c3071f8dd6b36cf9
ZiwaTu <ziwa5656@gmail.com>
2016-09-11 01:02:05 +0800

    Update DataBaseConn.java

--- src/tw/iii/qr/DataBaseConn.java
+++ src/tw/iii/qr/DataBaseConn.java
@@ -12,7 +12,7 @@ public class DataBaseConn {
 	private Connection conn;
 	private boolean isclosed;
 	public Connection getConn() throws SQLException, Exception, IllegalAccessException, ClassNotFoundException {
-<<<<<<< HEAD
+
 
 
 
@@ -21,13 +21,7 @@ public class DataBaseConn {
 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
 
 
-=======
 
- 		String urlstr = "jdbc:sqlserver://qrdata.database.windows.net:1433;database=qrdata;user=qruser;password=P@ssword;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30";
-
-
- 		Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver").newInstance();
->>>>>>> refs/remotes/origin/master
 
 		conn = DriverManager.getConnection(urlstr);
 		isclosed = !conn.isClosed();
