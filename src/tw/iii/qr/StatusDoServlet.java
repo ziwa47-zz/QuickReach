8382d66355a31f099a4668a012a4586a3ddda77c
ZiwaTu <ziwa5656@gmail.com>
2016-10-07 15:34:22 +0800

    update

--- src/tw/iii/qr/StatusDoServlet.java
+++ src/tw/iii/qr/StatusDoServlet.java
@@ -183,8 +183,8 @@ public class StatusDoServlet extends HttpServlet {
 				OFactory.isBundledeductStock(request, conn);
 				OFactory.insertIntoShippingLog(request, conn);
 				OFactory.insertIntoPurchaseLogFromOrders(request, conn);
-//				CompleteSale myCompleteSale = new CompleteSale();
-//				myCompleteSale.CompleteSale1(request);
+				CompleteSale myCompleteSale = new CompleteSale();
+				myCompleteSale.CompleteSale1(request);
 				response.sendRedirect("QROrders/OrderFinished.jsp?begin=0&end=10");
 				conn.close();
 			} else {
