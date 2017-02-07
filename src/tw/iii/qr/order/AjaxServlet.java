a0f264b1c40b2e5bf8842e1a8899b6a28a32bed3
sklack <sklackmn@gmail.com>
2017-02-07 18:06:23 +0800

    Jenan 2017/02/07 temprary sync
    Jenan 2017/02/07 temprary sync

---
+++ src/tw/iii/qr/order/AjaxServlet.java
@@ -0,0 +1,72 @@
+package tw.iii.qr.order;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import tw.iii.qr.DataBaseConn;
+import tw.iii.qr.order.DTO.COrderCombineFactory;
+import tw.iii.qr.order.DTO.COrderFactory;
+
+/**
+ * Servlet implementation class AjaxServlet
+ */
+@WebServlet("/AjaxServlet")
+public class AjaxServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public AjaxServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		try {
+			processAjax(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		//doGet(request, response);
+		try {
+			processAjax(request, response);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private void processAjax(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		HttpSession session = request.getSession();
+		Connection conn = new DataBaseConn().getConn();
+		COrderCombineFactory OCFactory = new COrderCombineFactory();
+		
+	}
+
+}
