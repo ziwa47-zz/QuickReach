cdab97482a82baa48c7ae8ce8ae0ac5e7da423ea
whateverc <verlander243@gmail.com>
2017-02-28 16:11:50 +0800

    生bean工具

---
+++ src/tw/iii/tool/BeanGenerator.java
@@ -0,0 +1,9 @@
+package tw.iii.tool;
+
+import lazy2.tool.beangenerator.gui.BeanGeneratorGUI;
+
+public class BeanGenerator {
+	public static void main(String[] args) {
+		BeanGeneratorGUI.main(args);
+	}
+}
